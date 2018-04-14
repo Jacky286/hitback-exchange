53917e3ca783499681b08109e11e0267f6690871
Jack <jack@King-MBP.lan>
2018-04-15 03:01:36 +0800

    add tradingview to replace K chart

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,6 @@
 Rails.application.config.assets.precompile += %w( html5.js )
 Rails.application.config.assets.precompile += %w( market.css )
 Rails.application.config.assets.precompile += %w( market.js )
+Rails.application.config.assets.precompile += %w( charting_library/charting_library.min.js )
+Rails.application.config.assets.precompile += %w( datafeeds/udf/bundle.js )
+Rails.application.config.assets.precompile += %w( datafeeds/udf/polyfills.js )
